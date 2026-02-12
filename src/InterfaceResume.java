5637fac9b203e31b3d437398e3748101c1f4ce6b
AkhilCet <tve24el005@cet.ac.in>
2026-02-12 19:30:55 +0530

    Update InterfaceResume.java

--- src/InterfaceResume.java
+++ src/InterfaceResume.java
@@ -18,7 +18,7 @@ class Teacher implements Resume {
     public void displayInfo() {
         System.out.println("Name: " + name);
         System.out.println("Qualification: " + subject);
-        System.out.println("Experience Years: " + experienceYears);
+        System.out.println("Experience: " + experienceYears);
     }
 
 }
